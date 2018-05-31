d6fad61b316a0c6148c99c2fb2b76635c4eebc40
jo <jonathan.deflaugergues@iwecloud.com>
2018-05-31 11:37:10 +0200

    configure route parameters

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,6 +10,7 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
+      1,
       'Tasty Schnitzel',
       'A super-tasty Schnitzel - just awesome!',
       'https://previews.123rf.com/images/foodandmore/foodandmore1502/foodandmore150200150/36578073-close-up-tasty-recipe-of-crumbled-escalope-with-potato-fries-styled-with-slice-of-lemon-and-tomato-a.jpg',
@@ -18,6 +19,7 @@ export class RecipeService {
         new Ingredient('French Fries', 20)
       ]),
     new Recipe(
+      2,
       'Big Fat Burger',
       'What else you need to say?',
       'https://i.pinimg.com/236x/48/f6/e9/48f6e92bbb500ea47f94ad752a09f425.jpg',
@@ -31,6 +33,10 @@ export class RecipeService {
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(id: number) {
+    return this.recipes.find((recipe: Recipe) => recipe.id === id);
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
